3f47cff35787f7cdaf6215637dd06f17b37322e9
Davit Babayan <davit_babayan@epam.com>
2025-02-06 20:30:50 +0400

    code style fix

--- webui/modalLangs.js
+++ webui/modalLangs.js
@@ -1,4 +1,4 @@
-    Array.from(document.querySelectorAll('.voice-btn')).forEach(el => {
+Array.from(document.querySelectorAll('.voice-btn')).forEach(el => {
     let langElem = el.parentElement.parentElement.parentElement.parentElement.parentElement
 
     let code = langElem.querySelector('code').innerText
@@ -6,7 +6,6 @@
     let voice = el.parentElement.parentElement.parentElement.innerText.split('\n')[0]
 
     let modelID = `${code}-${voice}-${quality}`
-
 
     el.addEventListener('click', e => {
         let links = []
