abab20e6740d64fe5425b7dd85260c1ab26bbe0f
Davit Babayan <davit_babayan@epam.com>
2025-02-07 14:47:31 +0400

    Download model file

--- utils.go
+++ utils.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
@@ -32,4 +35,61 @@ func fixChunkSplit(text string) string {
 	// ToDo
 
 	return text
+}
+
+func fileNameFromUrl(url string) string {
+	path := strings.Split(url, "?")[0]
+	return filepath.Base(path)
+
+}
+
+func downloadModelFile(url string) error {
+	// Create a temporary file
+	tmpFile, err := os.CreateTemp("", "download-*.tmp")
+	if err != nil {
+		return fmt.Errorf("failed to create temp file: %w", err)
+	}
+	defer os.Remove(tmpFile.Name())
+
+	// Get the final path for the file
+	finalPath := fileNameFromUrl(url)
+
+	// Download the file
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("failed to download file: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// Create a buffer to read chunks
+	buf := make([]byte, 1024*1024) // 1MB buffer
+
+	// Write the downloaded content to the temporary file in chunks
+	for {
+		n, err := resp.Body.Read(buf)
+		if err != nil && err != io.EOF {
+			return fmt.Errorf("failed to read response body: %w", err)
+		}
+		if n == 0 {
+			break
+		}
+
+		if _, err := tmpFile.Write(buf[:n]); err != nil {
+			return fmt.Errorf("failed to write to temp file: %w", err)
+		}
+	}
+
+	// Close the temporary file
+	err = tmpFile.Close()
+	if err != nil {
+		return fmt.Errorf("failed to close temp file: %w", err)
+	}
+
+	// Move the temporary file to the final location
+	err = os.Rename(tmpFile.Name(), filepath.Join(baseDir(), "models", finalPath))
+	if err != nil {
+		return fmt.Errorf("failed to move file to final location: %w", err)
+	}
+
+	return nil
 }
