1b464a77d3389497dd3cf2c148ab9083ecbf5f36
Davit Babayan <davit_babayan@epam.com>
2025-02-08 09:47:17 +0400

    popup on download or remove

--- webui/modalLangs.js
+++ webui/modalLangs.js
@@ -13,6 +13,11 @@ function modelActionFailure(message = '') {
     location.reload()
 }
 
+function showProgress() {
+    var modal = document.getElementById("progressModal")
+    modal.style.display = "block"
+}
+
 Array.from(document.querySelectorAll('.voice-btn')).forEach(el => {
     let langElem = el.parentElement.parentElement.parentElement.parentElement.parentElement
 
@@ -25,25 +30,31 @@ Array.from(document.querySelectorAll('.voice-btn')).forEach(el => {
 
     if (models.indexOf(modelID) !== -1) {
         el.innerText = 'Remove'
+        el.classList.add('remove-model')
         action = 'remove'
     } else {
         el.innerText = 'Download'
+        el.classList.add('download-model')
         action = 'download'
     }
 
     el.addEventListener('click', e => {
+
         if (action == 'download'){
             let links = []
             Array.from(e.target.parentElement.querySelectorAll('span')).forEach(el => {
                 links.push(el.dataset.href)
             })
 
+            showProgress()
             countdown = links.length
             links.forEach(link => onModelAction(action, link))
         } else if (action == 'remove') {
             if(!confirm("Do you want to delete model: \n" + modelID)){
                 return
             }
+
+            showProgress()
             countdown = 2
             onModelAction(action, modelID+'.onnx')
             onModelAction(action, modelID+'.onnx.json')
